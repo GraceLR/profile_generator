0797f98effb54fdabf4e568ffb8a7f8075f9ce11
GraceLR <gracerwork08@gmail.com>
2021-11-18 16:39:48 +0000

    update

--- survey.js
+++ survey.js
@@ -9,33 +9,31 @@ const rl = readline.createInterface({
 
 rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer) => {
 
-    let name = answer;
+  rl.question('What\'s an activity you like doing? ', () => {
 
-    rl.question('What\'s an activity you like doing? ', (answer) => {
-
-        rl.question('What do you listen to while doing that? ', (answer) => {   
+    rl.question('What do you listen to while doing that? ', () => {
         
-            rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {   
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', () => {
         
-                rl.question('What\'s your favourite thing to eat for that meal? ', (answer) => {   
+        rl.question('What\'s your favourite thing to eat for that meal? ', () => {
         
-                    rl.question('Which sport is your absolute favourite? ', (answer) => {   
+          rl.question('Which sport is your absolute favourite? ', () => {
         
-                        rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {   
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', () => {
         
-                            console.log(`Thank you for your valuable feedback: ${name}`);
-                            rl.close();
-        
-                        });
-        
-                    });
-        
-                });
+              console.log(`Thank you for your valuable feedback: ${answer}`);
+              rl.close();
         
             });
         
+          });
+        
         });
         
+      });
+        
     });
+        
+  });
 
 });
